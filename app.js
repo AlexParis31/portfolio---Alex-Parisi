909ef928d3bb1b8be2b2adc41a2b3fd360f90ff5
Alexander Parisi <alexanderparisi@Alexanders-MacBook-Air-2.local>
2023-03-01 11:42:48 -0500

    almost ready

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ $(() => {
     const $hover = $('.sideBar')
 
     const appear = () => {
-    $('.buttons').css('display', 'block')
+    $('.buttons').show('slow')
+
     }
 
     const disappear = () => {
@@ -25,6 +26,7 @@ $(() => {
         $('#resume').css('display', 'none')
         $('#projects').css('display', 'none')
         $('.carousel-images').children().eq(imgIndex).css('display','block')
+        $('header').text("Alex's Biography")
     }
 
     const $clickResume = $('.resumeButton')
@@ -34,6 +36,7 @@ $(() => {
         $('#home-page').css('display','none')
         $('#bio').css('display', 'none')
         $('#projects').css('display', 'none')
+        $('header').text("Alex's Resume")
     }
 
     const $clickProjects = $('.projectsButton')
@@ -43,6 +46,7 @@ $(() => {
         $('#home-page').css('display','none')
         $('#bio').css('display', 'none')
         $('#resume').css('display', 'none')
+        $('header').text("Alex's Project")
     }
 
     const $clickHome = $('.homeButton')
@@ -52,6 +56,7 @@ $(() => {
         $('#bio').css('display', 'none')
         $('#resume').css('display', 'none')
         $('#projects').css('display', 'none')
+        $('header').text("Alex's Website")
     }
     // ** buttons to other pages **
 
@@ -59,9 +64,22 @@ $(() => {
 
 // Section 3 carousel stuff
 
+    const $clickPrevious = $('#previous')
+
     const $clickNext = $('#next')
 
     let imgIndex = 0
+
+    const previousButton = () => {
+        imgIndex = imgIndex-1
+        $('.carousel-images').children().eq(imgIndex + 1).css('display','none')
+        if (imgIndex < 0) {
+            imgIndex = 2
+            
+        }
+        $('.carousel-images').children().eq(imgIndex).css('display','block')
+    }
+
     const nextButton = () => {
         imgIndex ++
         $('.carousel-images').children().eq(imgIndex - 1).css('display','none')
@@ -91,7 +109,7 @@ $(() => {
 
     const projectOne = () => {
     const $projectOneText = $('.project-text').eq(0)
-    $projectOneText.css('display','block')
+    $projectOneText.slideDown('slow')
     $clickProjectOne.css('display','none')
     $clickProjectOneHide.css('display','inline-block')
 
@@ -99,14 +117,14 @@ $(() => {
 
     const projectTwo = () => {
     const $projectTwoText = $('.project-text').eq(1)
-    $projectTwoText.css('display','block')
+    $projectTwoText.slideDown('slow')
     $clickProjectTwo.css('display','none')
     $clickProjectTwoHide.css('display','inline-block')
     }
 
     const projectThree = () => {
     const $projectThreeText = $('.project-text').eq(2)
-    $projectThreeText.css('display','block')
+    $projectThreeText.slideDown('slow')
     $clickProjectThree.css('display','none')
     $clickProjectThreeHide.css('display','inline-block')
     }
@@ -115,21 +133,21 @@ $(() => {
 
     const projectOneHide = () => {
         const $projectOneText = $('.project-text').eq(0)
-        $projectOneText.css('display','none')
+        $projectOneText.slideUp('slow')
         $clickProjectOne.css('display','inline-block')
         $clickProjectOneHide.css('display','none')
         }
 
     const projectTwoHide = () => {
         const $projectTwoText = $('.project-text').eq(1)
-        $projectTwoText.css('display','none')
+        $projectTwoText.slideUp('slow')
         $clickProjectTwo.css('display','inline-block')
         $clickProjectTwoHide.css('display','none')
         }
 
     const projectThreeHide = () => {
         const $projectThreeText = $('.project-text').eq(2)
-        $projectThreeText.css('display','none')
+        $projectThreeText.slideUp('slow')
         $clickProjectThree.css('display','inline-block')
         $clickProjectThreeHide.css('display','none')
         }
@@ -137,10 +155,46 @@ $(() => {
 
     // ** Project Page **
 
+// Section 5 Opacity on buttons hover 
+const $hoverBio = $('.bioButton')
 
+const opacityBio = () => {
+$hoverBio.css('opacity', '0.8')
+}
+const noOpacityBio = () => {
+    $hoverBio.css('opacity', '1')
+}
+
+const $hoverResume = $('.resumeButton')
+
+const opacityResume = () => {
+$hoverResume.css('opacity', '0.8')
+}
+const noOpacityResume = () => {
+    $hoverResume.css('opacity', '1')
+}
+
+const $hoverProjects = $('.projectsButton')
+
+const opacityProjects = () => {
+$hoverProjects.css('opacity', '0.8')
+}
+const noOpacityProjects = () => {
+    $hoverProjects.css('opacity', '1')
+}
+
+const $hoverHome = $('.homeButton')
+
+const opacityHome = () => {
+$hoverHome.css('opacity', '0.8')
+}
+const noOpacityHome = () => {
+    $hoverHome.css('opacity', '1')
+}
 
 // CLICKS:
 
+    
 //Section 1: 
     $hover.hover(appear, disappear )
 //Section 2: 
@@ -149,6 +203,7 @@ $(() => {
     $clickProjects.on('click', projectsAppear)
     $clickHome.on('click', homePageAppear)
 //Section 3
+    $clickPrevious.on('click', previousButton)
     $clickNext.on('click', nextButton)
 //Section 4
     $clickProjectOne.on('click', projectOne)
@@ -159,6 +214,11 @@ $(() => {
     $clickProjectTwoHide.on('click', projectTwoHide)
     $clickProjectThreeHide.on('click', projectThreeHide)
 
+// Section 5
+    $hoverBio.hover(opacityBio, noOpacityBio)
+    $hoverResume.hover(opacityResume, noOpacityResume)
+    $hoverProjects.hover(opacityProjects, noOpacityProjects)
+    $hoverHome.hover(opacityHome, noOpacityHome)
     
 
     
